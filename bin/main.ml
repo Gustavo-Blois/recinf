13be5e7e8742a4eb128c857b0a0278c92f46b94c
Gustavo Blois <gustavobloisrr@gmail.com>
2026-09-10 16:27:29 -0300

    token logic and first commit

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,2 @@
-let () = print_endline "Hello, World!"
+let () = print_endline @@ String.concat "!!!!" @@ Recinf.Token.tokenize "Hello, World!"
+
---
+++ lib/token.ml
@@ -0,0 +1,11 @@
+let punctuations = ",.;!?()'\"\\/{}[]@#$%:\n\t\r"
+let match_punctuations c = 
+  String.exists (Char.equal c) punctuations 
+
+let filter_string pred s = 
+  String.to_seq s |> Seq.filter (fun c -> not (pred c)) |> String.of_seq
+
+
+let tokenize t = filter_string match_punctuations t |> String.split_all ~sep:" " |> List.filter (fun c -> not @@ String.equal c "")
+
+
